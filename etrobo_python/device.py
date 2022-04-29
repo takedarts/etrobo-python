2227948978b5976cdb9ed585f4580ae2c3b5ab80
Atsushi TAKEDA <atsushi@takedarts.jp>
2022-04-30 01:29:57 +0900

    コメントを追加

--- etrobo_python/device.py
+++ etrobo_python/device.py
@@ -6,34 +6,80 @@ except BaseException:
 
 class Motor(object):
     def get_count(self) -> int:
+        '''モーターの回転角度を返す。
+
+        Returns:
+          モーターの回転角度（単位はdeg）。
+        '''
         raise NotImplementedError()
 
     def set_pwm(self, pwm: int) -> None:
+        '''モーターのPWMの値を設定する。
+
+        Args:
+          pwm: PWMの設定値。
+        '''
         raise NotImplementedError()
 
     def set_brake(self, brake: bool) -> None:
+        '''ブレーキモードを設定する。
+
+        Args:
+          brake: Trueを指定した場合はブレーキモードとなる。
+        '''
         raise NotImplementedError()
 
 
 class ColorSensor(object):
     def get_brightness(self) -> int:
+        '''反射光の測定値を返す。
+
+        Returns:
+          反射光の測定値。
+        '''
         raise NotImplementedError()
 
     def get_ambient(self) -> int:
+        '''環境光の測定値を返す。
+
+        Returns:
+          環境光の測定値
+        '''
         raise NotImplementedError()
 
     def get_raw_color(self) -> Tuple[int, int, int]:
+        '''RGBの測定値を返す。
+
+        Returns:
+          測定された(Red, Green, Blue)の値。
+        '''
         raise NotImplementedError()
 
 
 class TouchSensor(object):
     def is_pressed(self) -> bool:
+        '''タッチセンサの測定値を返す。
+
+        Returns:
+          タッチセンサが押されていればTrue。
+        '''
         raise NotImplementedError()
 
 
 class SonarSensor(object):
     def listen(self) -> bool:
+        '''超音波信号の受信状況を返す。
+
+        Returns:
+          超音波信号を受信できていればTrue。
+        '''
         raise NotImplementedError()
 
     def get_distance(self) -> int:
+        '''超音波信号によって測定された物体までの距離を返す。
+        超音波信号が受信できていない場合の値は不定。
+
+        Returns:
+          超音波信号によって測定された物体までの距離（単位はmm）。
+        '''
         raise NotImplementedError()
--- etrobo_python/etrobo.py
+++ etrobo_python/etrobo.py
@@ -6,6 +6,17 @@ except BaseException:
 
 class ETRobo(object):
     def __init__(self, backend: str) -> None:
+        '''ロボットを制御するためのオブジェクトを作成する。
+        実行環境に適したバックエンドプログラムを指定すること。
+
+        バックエンドプログラム:
+          simulator: Unityのシミュレータ環境でのロボット制御
+          pybricks: micropythonを使ったEV3ロボットの制御
+          raspike: micropythonを使ったRasPikeロボットの制御（未実装）
+
+        Args:
+          backend: バックエンドプログラムの名前
+        '''
         if backend == 'simulator':
             from .backends import simulator
             self.backend = simulator  # type: Any
@@ -20,12 +31,46 @@ class ETRobo(object):
         self.handlers = []  # type: List[Callable[..., None]]
 
     def add_device(self, name: str, device_type: str, port: str) -> 'ETRobo':
+        '''制御対象となるデバイスを登録する。
+        ここで登録されたデバイスオブジェクトは制御ハンドラに引数として渡される。
+
+        制御デバイスは以下のものをサポートしている:
+          motor, color_sensor, touch_sensor, sonar_sensor
+
+        例:
+          motorタイプの制御デバイス motor1 を登録した場合、
+          制御ハンドラにはMotorオブジェクトが引数 motor1 として渡される。
+
+          def motor_handler(motor1: etrobo_python.Motor) -> None:
+              ...
+
+          etrobo = ETRobo(backend)
+          etrobo.add_device('motor1', device_type='motor', port='A')
+          etrobo.add_handler(motor_handler)
+
+        Args:
+          name: 制御デバイスの名前（handlerに渡される引数名）。
+          device_type: 制御デバイスの種類
+          port: 制御デバイスを接続しているポート
+
+        Returns:
+          このオブジェクト
+        '''
         device = self.backend.create_device(device_type, port)
         self.devices.append((name, device))
         return self
 
-    def add_handler(self, func: Callable[..., None]) -> 'ETRobo':
-        self.handlers.append(func)
+    def add_handler(self, handler: Callable[..., None]) -> 'ETRobo':
+        '''制御ハンドラを登録する。
+        ここで登録された制御ハンドラは、制御プログラム開始後に指定された間隔で実行される。
+
+        Args:
+          handler: 制御ハンドラ
+
+        Returns:
+          このオブジェクト
+        '''
+        self.handlers.append(handler)
         return self
 
     def dispatch(self, **kwargs) -> None:
