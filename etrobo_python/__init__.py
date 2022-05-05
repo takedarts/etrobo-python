39c046c555d0430b94d3989f9470873738076bb5
Atsushi TAKEDA <atsushi@takedarts.jp>
2022-05-06 08:02:21 +0900

    バージョン番号と開発者名を追加

--- etrobo_python/__init__.py
+++ etrobo_python/__init__.py
@@ -1,3 +1,6 @@
 from .device import (ColorSensor, GyroSensor, Hub, Motor, SonarSensor,  # noqa
                      TouchSensor)
 from .etrobo import ETRobo  # noqa
+
+__author__ = 'Atsushi TAKEDA'
+__version__ = '0.2'
