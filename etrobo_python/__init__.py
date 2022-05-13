162f006ea9c98663e310d164bc6ff7bc55262a63
Atsushi TAKEDA <atsushi@takedarts.jp>
2022-05-14 04:13:41 +0900

    バージョン番号を更新

--- etrobo_python/__init__.py
+++ etrobo_python/__init__.py
@@ -3,4 +3,4 @@ from .device import (ColorSensor, GyroSensor, Hub, Motor, SonarSensor,  # noqa
 from .etrobo import ETRobo  # noqa
 
 __author__ = 'Atsushi TAKEDA'
-__version__ = '0.2'
+__version__ = '0.3'
