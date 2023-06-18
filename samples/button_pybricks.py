8a8cd9d89fe2f42963199f1178db6a88d10e6d10
Atsushi TAKEDA <atsushi@takedarts.jp>
2023-06-18 20:20:58 +0900

    本体のボタンを押すことでモーターを制御するサンプルコードを追加

---
+++ samples/button_pybricks.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env pybricks-micropython
+from button_simulator import run
+
+if __name__ == '__main__':
+    run(backend='pybricks')
---
+++ samples/button_simulator.py
@@ -0,0 +1,41 @@
+'''ボタンによってモーターを操作するプログラム。
+左ボタンを押す：左方向に旋回する
+右ボタンを押す：右方向に旋回する
+上ボタンを押す：前進する
+下ボタンを押す：後退する
+'''
+from etrobo_python import ETRobo, Hub, Motor
+
+MOTOR_POWER = 50
+
+
+class Controller(object):
+    def __call__(self, hub: Hub, left_motor: Motor, right_motor: Motor) -> None:
+        if hub.is_left_button_pressed():
+            left_motor.set_power(MOTOR_POWER)
+            right_motor.set_power(-MOTOR_POWER)
+        elif hub.is_right_button_pressed():
+            left_motor.set_power(-MOTOR_POWER)
+            right_motor.set_power(MOTOR_POWER)
+        elif hub.is_up_button_pressed():
+            left_motor.set_power(MOTOR_POWER)
+            right_motor.set_power(MOTOR_POWER)
+        elif hub.is_down_button_pressed():
+            left_motor.set_power(-MOTOR_POWER)
+            right_motor.set_power(-MOTOR_POWER)
+        else:
+            left_motor.set_power(0)
+            right_motor.set_power(0)
+
+
+def run(backend: str, **kwargs) -> None:
+    (ETRobo(backend=backend)
+     .add_hub('hub')
+     .add_device('right_motor', device_type=Motor, port='B')
+     .add_device('left_motor', device_type=Motor, port='C')
+     .add_handler(Controller())
+     .dispatch(**kwargs))
+
+
+if __name__ == '__main__':
+    run(backend='simulator')
